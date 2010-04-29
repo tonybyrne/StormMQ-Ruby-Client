d3554ef511db5b4106dd91412345fd03f47f1d2a
Tony Byrne <tonyb@byrnehq.com>
2010-04-29 11:37:47 +0100

    Add RDoc documentation.

--- lib/stormmq/secret_keys.rb
+++ lib/stormmq/secret_keys.rb
@@ -18,20 +18,25 @@ module StormMQ
     include Singleton
     attr_writer :key_cache
 
+    # Returns the base64 encoded secret key for the given user name from the secret keys file.
     def key_for(user)
       keys[user]
     end
 
+    # Returns an <tt>Array</tt> of user names of users who have keys in the secret keys file.
     def users
       keys.keys
     end
 
     private
 
+    # Return a hash of keys stored in the secret keys file.
     def keys
       @keys_cache ||= load_secret_keys
     end
 
+    # Load the keys from the secret keys file <tt>keyfile</tt>.  Walks the locations specified in
+    # <tt>search_path</tt> in order of preference.
     def load_secret_keys(search_path=SECRET_KEYS_SEARCH_PATH, keyfile=SECRET_KEYS_FILENAME)
       full_paths = search_path.map{|p| File.expand_path("#{p}/#{keyfile}")}
       full_paths.each do |full_path|
@@ -45,6 +50,7 @@ module StormMQ
           caller
     end
 
+    # Create a hash of the keys contained in json fragment.
     def self.secret_keys_hash_from_json(json_string)
       hash = JSON.parse(json_string)
       hash.values.each {|v| v.tr!("-_","+/")}
